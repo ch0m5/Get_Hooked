524f2fead31f569318bc1563fb15cf5d217991d2
Samuel Kurt Davidson <36154071+samuelkurtdavidson@users.noreply.github.com>
2018-11-18 02:10:17 +0100

    added backgrounds and completed maps

--- Get Hooked/Motor2D/j1Map.cpp
+++ Get Hooked/Motor2D/j1Map.cpp
@@ -298,7 +298,7 @@ bool j1Map::LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set)
 bool j1Map::LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set)
 {
 	bool ret = true;
-	pugi::xml_node image = tileset_node.child("image");
+	pugi::xml_node image = tileset_node.child("image");	
 
 	if(image == NULL)
 	{
